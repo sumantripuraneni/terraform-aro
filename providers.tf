62b65fe142742dce34b064c0bd63cc1f722c8253
Suman Tripuraneni <suman.tripuraneni@gmail.com>
2023-11-04 10:26:00 -0400

    added provider for backend

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "openenv-t74hk"
-    storage_account_name = "azuredemosuman"
+    resource_group_name  = "openenv-9rpv6"
+    storage_account_name = "azuredemosuman1"
     container_name       = "tfstate"
     key                  = "terraform-base.tfstate"
   }
